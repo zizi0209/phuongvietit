8cafa61cd75d54733494993f7ed1dd53a9ccef6d
Cole Mitchell <colemitchell951@msn.com>
2025-08-30 23:42:53 +0700

    deploy 20

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 // next.config.js
-const isPages = process.env.GITHUB_ACTIONS === "true";
-const repo = "TEN_REPO_CUA_BAN"; // đổi thành tên repo
+const isCI = process.env.GITHUB_ACTIONS === "true";
+const repo = isCI ? process.env.GITHUB_REPOSITORY?.split("/")[1] : "";
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
-  images: { unoptimized: true },
-  basePath: isPages ? `/${repo}` : "",
-  assetPrefix: isPages ? `/${repo}/` : "",
-  trailingSlash: true,
+  images: { unoptimized: true }, // bắt buộc khi export
+  basePath: isCI ? `/${repo}` : "", // ví dụ: /phuongvietit
+  assetPrefix: isCI ? `/${repo}/` : "",
+  trailingSlash: true, // giúp Pages phục vụ route tĩnh tốt hơn
 };
