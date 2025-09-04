dc63d974b727bb39ae96a43a9882061261d4412e
Zyth <colemitchell951@msn.com>
2025-09-04 08:17:34 +0700

    deploy2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/phuongvietit", // tên repo
+  assetPrefix: "/phuongvietit/",
 };
 
 export default nextConfig;
